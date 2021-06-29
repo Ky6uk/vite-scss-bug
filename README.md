dbada0ff3c9c1b6f23cd90a92b7cd2e5ffbf5b60
Roman Nuritdinov (Ky6uk) <ky6uk.kun@gmail.com>
2021-06-29 16:32:09 +0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+1. clone
+2. `npm install`
+3. `vite build`
+4. `cat dist/assets/index.*.css`
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    Woof
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld'
+})
+</script>
+
+<style scoped>
+  .test {
+    background:
+      no-repeat bottom right/166px url("@/static/img/test.png"),
+      linear-gradient(45deg, #4978EA 1.56%, #A09FC3 111.41%);
+  }
+</style>
